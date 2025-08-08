0e130bea68b822567fa6bde7957da68e481591ac
Manuvg1907 <manuvgcs26@gmail.com>
2025-08-08 22:43:37 +0530

    Update index.jsx
    updated to match necessary rootings

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// Global CSS imports
+import "bootstrap/dist/css/bootstrap.min.css";
+import "animate.css";
+import "react-toastify/dist/ReactToastify.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
